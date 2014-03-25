96e078d2b8212c3441677812e4ab1efe4d84af88
LenaSavantic <lena@savantic.se>
2014-03-25 13:37:11 +0100

    Added the first couple of GUI pages

--- gen/com/savanticab/seaweedapp/R.java
+++ gen/com/savanticab/seaweedapp/R.java
@@ -10,11 +10,54 @@ package com.savanticab.seaweedapp;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Example customization of dimensions originally defined in res/values/dimens.xml
+         (such as screen margins) for screens with more than 820dp of available width. This
+         would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int seaweed_center_logo=0x7f020001;
+        public static final int seaweed_center_logo_2=0x7f020002;
+    }
+    public static final class id {
+        public static final int button_factory_click=0x7f070007;
+        public static final int button_report=0x7f07000a;
+        public static final int button_report_click=0x7f07000b;
+        public static final int button_shop_click=0x7f070009;
+        public static final int button_to_factory=0x7f070006;
+        public static final int button_to_shop=0x7f070008;
+        public static final int container=0x7f070000;
+        public static final int editText1=0x7f070003;
+        public static final int imageView1=0x7f070005;
+        public static final int item_detail=0x7f070004;
+        public static final int item_detail_container=0x7f070001;
+        public static final int item_list=0x7f070002;
+    }
+    public static final class layout {
+        public static final int activity_fullscreen=0x7f030000;
+        public static final int activity_item_detail=0x7f030001;
+        public static final int activity_item_list=0x7f030002;
+        public static final int activity_item_twopane=0x7f030003;
+        public static final int fragment_item_detail=0x7f030004;
+        public static final int fragment_main=0x7f030005;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int action_settings=0x7f050003;
+        public static final int app_name=0x7f050000;
+        public static final int at_the_factory=0x7f050001;
+        public static final int hello_world=0x7f050002;
+        public static final int seaweed_center_logo=0x7f050004;
+        public static final int start_to_factory=0x7f050007;
+        public static final int start_to_reports=0x7f050005;
+        public static final int start_to_shop=0x7f050006;
+        public static final int title_item_detail=0x7f050008;
     }
     public static final class style {
         /** 
@@ -38,10 +81,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f060001;
     }
 }
